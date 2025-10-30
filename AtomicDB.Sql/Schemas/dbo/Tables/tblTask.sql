a0e997079ea66f757cfbdac75a17d1403d9e10ff
Chris Waterhouse <C7Waterhouse@gmail.com>
2025-10-30 21:44:11 +0000

    Added SQL Project tables with Task and User tables and publish file for deploying to the Local Express SQL database

---
+++ AtomicDB.Sql/Schemas/dbo/Tables/tblTask.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblTask]
+(
+	[TaskId]			INT					NOT NULL	IDENTITY(1,1) CONSTRAINT [PK_tblTask_TaskId] PRIMARY KEY CLUSTERED([TaskId] ASC) ON [PRIMARY],
+	[Title]				NVARCHAR(100)		NOT NULL,
+	[Description]		NVARCHAR(Max)		NULL,
+	[DueDate]           DATETIME2 (0)		NULL,
+	[AssignedToUserId]	INT					NULL		CONSTRAINT [FK_tblTask_AssignedToUserId_tblUser_UserId] FOREIGN KEY (AssignedToUserId) REFERENCES [dbo].[tblUser] (UserId),
+	[IsCompleted]		BIT					NOT NULL	CONSTRAINT [DF_tblUser_IsCompleted] DEFAULT ((0)),
+	[CompletedDate]		DATETIME2 (0)		NULL,
+	[IsCancelled]		BIT					NOT NULL	CONSTRAINT [DF_tblUser_IsCancelled] DEFAULT ((0)),
+	[CancelledDate]		DATETIME2 (0)		NULL,
+	[CreatedDate]		DATETIME2			NOT NULL	CONSTRAINT [DF_tblTask_CreatedDate] DEFAULT (GETUTCDATE()),
+	[CreatedByUserId]	INT					NOT NULL	CONSTRAINT [FK_tblTask_CreatedByUserId_tblUser_UserId] FOREIGN KEY (CreatedByUserId) REFERENCES [dbo].[tblUser] (UserId),
+	[LastModifiedDate]	DATETIME2			NOT NULL	CONSTRAINT [DF_tblTask_LastModifiedDate] DEFAULT (GETUTCDATE()),
+	[EntityId]			UNIQUEIDENTIFIER	NOT NULL	DEFAULT (newid())
+)
---
+++ AtomicDB.Sql/Schemas/dbo/Tables/tblUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblUser] (
+    [UserId]        INT              IDENTITY (10000, 1) NOT FOR REPLICATION NOT NULL,
+    [Name]          NVARCHAR (100)   NOT NULL,
+    [EntityId]      UNIQUEIDENTIFIER NOT NULL   DEFAULT (newid()),
+    [IsActive]		BIT				 NOT NULL	CONSTRAINT [DF_tblUser_IsActive] DEFAULT ((1)),
+    [CreatedDate]	DATETIME2	     NOT NULL	CONSTRAINT [DF_tblUser_CreatedDate] DEFAULT (GETUTCDATE()),
+    CONSTRAINT [PK_tblUser_UserId] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+GO
